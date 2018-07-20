797472132f811d75e7c7265e7487f4b3414023da
13592513537@163.com <sjh13592513537githup**>
2018-07-20 11:12:21 +0800

    dd

--- src/main/java/com/test/learnTableapi/TestMain3.java
+++ src/main/java/com/test/learnTableapi/TestMain3.java
@@ -2,15 +2,15 @@ package com.test.learnTableapi;
 
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.io.jdbc.JDBCAppendTableSink;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.KafkaTableSource;
-import org.apache.flink.table.api.Table;
-import org.apache.flink.table.api.TableEnvironment;
-import org.apache.flink.table.api.TableSchema;
-import org.apache.flink.table.api.TableSchemaBuilder;
+import org.apache.flink.table.api.*;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.api.java.Tumble;
+import org.apache.flink.table.plan.logical.LogicalWindow;
 import org.apache.flink.table.sinks.CsvTableSink;
 import org.apache.flink.table.sources.tsextractors.ExistingField;
 import org.apache.flink.table.sources.wmstrategies.BoundedOutOfOrderTimestamps;
@@ -21,19 +21,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+import static org.apache.flink.api.common.typeinfo.BasicTypeInfo.INT_TYPE_INFO;
+
 public class TestMain3 {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment sEnv = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tableEnv = TableEnvironment.getTableEnvironment(sEnv);
 
-        sEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
-
-        Map<String, TypeInformation> columnMap = new HashMap<>();
-        columnMap.put("_sysTime", Types.SQL_TIMESTAMP);
-        columnMap.put("sensorId", Types.LONG);
-        columnMap.put("ptime",Types.SQL_TIMESTAMP);
+//        sEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         Properties properties = new Properties();
-        properties.setProperty("input-topic","monitorBlocklyQueueKey5");
+        properties.setProperty("input-topic","monitorBlocklyQueueKey6");
         properties.setProperty("bootstrap.servers","172.31.24.30:9092");
         properties.setProperty("group.id","serverCollector");
 
@@ -43,24 +40,43 @@ public class TestMain3 {
         jsonTableSourceBuilder.withKafkaProperties(properties);
         // set Table schema
         TableSchemaBuilder tableSchemaBuilder= TableSchema.builder();
-        for (Map.Entry<String, TypeInformation> entry : columnMap.entrySet()) {
-            tableSchemaBuilder.field(entry.getKey(), entry.getValue());
-        }
 
-        jsonTableSourceBuilder.withSchema(tableSchemaBuilder.build());
-//        jsonTableSourceBuilder.withProctimeAttribute("ptime");
-        //values of "_sysTime" are at most out-of-order by 30 seconds
-        jsonTableSourceBuilder.withRowtimeAttribute("_sysTime", new ExistingField("_sysTime"),new BoundedOutOfOrderTimestamps(30000L));
+//        jsonTableSourceBuilder.withSchema(tableSchemaBuilder.field("a",Types.STRING).field("b",Types.INT).field("rtime",Types.SQL_TIMESTAMP).build()).withRowtimeAttribute("rtime",new ExistingField("rtime"),new BoundedOutOfOrderTimestamps(30000L));
+        jsonTableSourceBuilder.withSchema(tableSchemaBuilder.field("a",Types.STRING).field("b",Types.INT).field("rtime",Types.SQL_TIMESTAMP).build()).withProctimeAttribute("rtime");
 
         KafkaTableSource kafkaTableSource=jsonTableSourceBuilder.build();
 
+
         tableEnv.registerTableSource("kafkasource", kafkaTableSource);
 
+//        Table table = tableEnv.scan("kafkasource");
+//        Table sqlResult = table.window(Tumble.over("1.minutes").on(
+//"rtime").as("a")).groupBy("a").select("b.sum as num");
 
-        Table sqlResult = tableEnv.sql("select * from kafkasource");
+
+//        Table sqlResult = table.window(Tumble.over("10.rows").on("rtime").as("a")).groupBy("a").select("b.sum");
+
+//        StreamQueryConfig qConfig = new StreamQueryConfig();
+//
+
+
+
+        Table sqlResult = tableEnv.sqlQuery("SELECT count(a) FROM kafkasource");
+//        String insertSql="INSERT INTO testflink (num) VALUES (?)";
+//        JDBCAppendTableSink sink = JDBCAppendTableSink.builder()
+//                .setDrivername("org.postgresql.Driver")
+//                .setDBUrl("jdbc:postgresql://10.4.247.20:5432/apm_test")
+//                .setUsername("apm").setPassword("apm")
+//                .setQuery(insertSql)
+//                .setParameterTypes(INT_TYPE_INFO)
+//                .setBatchSize(1)
+//                .build();
+
+        CsvTableSink csvTableSink = new CsvTableSink("file:///E:\\Asunjihua\\idea\\LearnFlink\\src\\main\\resources\\aaaaaa.csv", ",", 1, FileSystem.WriteMode.OVERWRITE);
+//        csvTableSink.configure(new String[]{"sum"},new TypeInformation[]{Types.INT});
         //将数据写出去
 //        sqlResult.printSchema();
-        sqlResult.writeToSink(new CsvTableSink("file:///Users/apple/Documents/AgentJava/intellProject/LearnFlink/src/main/resources/aaaaaa.csv", ",", 1, FileSystem.WriteMode.OVERWRITE));
+        sqlResult.writeToSink(csvTableSink);
         //执行
         sEnv.execute();
     }
